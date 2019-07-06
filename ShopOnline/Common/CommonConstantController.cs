313f1435710c5e9862b3ead70c548320ca402dee
Nguyen Duc Thai <xichlong.it@gmail.com>
2019-07-06 13:06:53 +0700

    finish

---
+++ ShopOnline/Common/CommonConstantController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopOnline.Common
+{
+    public static class CommonConstant
+    {
+        // GET: CommonConstant
+        public static string USER_SESSION = "USER_SESSION";
+        public  static  string CartSession = "CartSession";
+    }
+}
---
+++ ShopOnline/Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopOnline.Models
+{
+    [Serializable] 
+    public class CartItem
+    {
+        
+        public SanPhamKH sanpham { get; set; }
+        public int soluong { get; set; }
+    }
+}
